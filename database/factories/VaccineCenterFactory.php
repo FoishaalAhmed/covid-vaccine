44f6f01e81683c92a03f858b86fe56d3a3279956
Foisal Ahmed <foisal.cse@gmail.com>
2024-10-09 21:54:12 +0600

    vaccine center seeder create

---
+++ database/factories/VaccineCenterFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\VaccineCenter;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\VaccineCenter>
+ */
+class VaccineCenterFactory extends Factory
+{
+    protected $model = VaccineCenter::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company,
+            'location' => $this->faker->address,
+            'visitor_limit' => $this->faker->numberBetween(1, 10),
+        ];
+    }
+}
